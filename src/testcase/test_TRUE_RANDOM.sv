588ddeb86269c2f1c9557b5491d053b38ad2e46f
GiaHuyNguyen13 <156430796+GiaHuyNguyen13@users.noreply.github.com>
2024-06-21 13:51:52 +0700

    Add files via upload

---
+++ src/testcase/test_TRUE_RANDOM.sv
@@ -0,0 +1,15 @@
+class test_TRUE_RANDOM extends base_test;
+  `uvm_component_utils(test_TRUE_RANDOM)
+  function new(string name="test_TRUE_RANDOM", uvm_component parent=null);
+    super.new(name, parent);
+  endfunction
+  
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    seq.randomize() with { 
+        num inside {[MIN_NUM:MAX_NUM]};
+        select_extension  inside {[0:5]};
+    };
+    
+  endfunction
+endclass
